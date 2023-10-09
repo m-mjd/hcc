12c0e0c2ea5eb5364c46f4b3af9c749b0eda8c19
m-mjd <120607317+m-mjd@users.noreply.github.com>
2023-10-09 06:03:08 -0700

    Update optimizer.sh

--- optimizer.sh
+++ optimizer.sh
@@ -5,11 +5,11 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RESET='\033[0m'
 ascii_art=(
-"${YELLOW} ||''''|                  ."
-" ||  .    ....    ....  .||.    ....  ... .."
-" ||''|   '' .||  ||. '   ||   .|...||  ||' ''"
-" ||      .|' ||  . '|..  ||   ||       ||"
-".||.     '|..'|' |'..|'  '|.'  '|...' .||.${RESET}"
+    "${YELLOW} ||''''|                  ."
+    " ||  .    ....    ....  .||.    ....  ... .."
+    " ||''|   '' .||  ||. '   ||   .|...||  ||' ''"
+    " ||      .|' ||  . '|..  ||   ||       ||"
+    ".||.     '|..'|' |'..|'  '|.'  '|...' .||.${RESET}"
 )
 
 for line in "${ascii_art[@]}"; do
@@ -20,20 +20,21 @@ if [[ $EUID -ne 0 ]]; then
     echo -e "${RED}This script must be run with the superuser. Please login with root user.${RESET}"
     exit 1
 fi
+
 action=${action:-install}
-echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n'
 read -p "Do you want to install or uninstall the cron job? (install/uninstall) [default: install]: " user_action
 action=${user_action:-$action}
 
 if [[ $action == install ]]; then
-   read -p "Enter the interval in hours to empty the cache (press Enter for default 2 hours): " time
-   time=${time:-2}
-   if [[ ! $time =~ ^[1-9][0-9]*$ ]]; then
-       echo -e "${RED}Invalid input. Please enter a positive integer for the time.${RESET}"
-       exit 1
-   fi
+    read -p "Enter the interval in hours to empty the cache (press Enter for default 2 hours): " time
+    time=${time:-2}
+    if [[ ! $time =~ ^[1-9][0-9]*$ ]]; then
+        echo -e "${RED}Invalid input. Please enter a positive integer for the time.${RESET}"
+        exit 1
+    fi
 
-   cron_command="*/$time * * * * echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'"
+    cron_command="*/$time * * * * echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'"
+    cron_command2="*/$time * * * * bash /opt/hiddify-config/apply_configs.sh"
 fi
 
 case "$action" in
@@ -46,11 +47,20 @@ case "$action" in
         fi
         ;;
     install)
+        echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n'
+        bash /opt/hiddify-config/apply_configs.sh
         if crontab -l | grep -q "$cron_command"; then
             echo -e "${YELLOW}The command is already installed in cron job.${RESET}"
         else
             (crontab -l 2>/dev/null; echo "$cron_command") | crontab -
             echo -e "${GREEN}The command has been successfully added to cron job.${RESET}"
+        fi
+
+        if crontab -l | grep -q "$cron_command2"; then  # Added block
+            echo -e "${YELLOW}The second command is already installed in cron job.${RESET}"
+        else
+            (crontab -l 2>/dev/null; echo "$cron_command2") | crontab -
+            echo -e "${GREEN}The second command has been successfully added to cron job.${RESET}"
         fi
         ;;
     *)
